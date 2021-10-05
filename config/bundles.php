4799dfa64adca658f849ceb0c3ad05bd8c63ffaf
Vlad Makar <vladislav.makar1@gmail.com>
2021-10-05 17:52:12 +0300

    Add `easy-doctrine` and `easy-error-handler` dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    EonX\EasyErrorHandler\Bridge\Symfony\EasyErrorHandlerSymfonyBundle::class => ['all' => true],
+    EonX\EasyDoctrine\Bridge\Symfony\EasyDoctrineSymfonyBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
